07c764ceae2dbe6a69e22722977fab8561928fcb
Hamza Aarab <haarab@c1r2p4.1337.ma>
2023-09-07 15:42:12 +0100

    fixing the export

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 13:10:25 by emohamed          #+#    #+#             */
-/*   Updated: 2023/09/06 20:04:21 by haarab           ###   ########.fr       */
+/*   Updated: 2023/09/07 15:29:40 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,62 +50,119 @@ int count_argiment(char **str)
 }
 
 
+// int check_argiment(char **args, t_vars *vars, int check)
+// {
+// 	int count = 0;
+// 	if (args[check])
+// 	{
+// 		int i = 0;
+// 		while (args[check][i])
+// 		{
+// 			if (args[check][i] == '=' && args[check][i + 1] != '\0')
+// 			{
+// 				count++;
+// 			}
+// 			i++;
+// 		}
+// 	}
+// 	int j = 0;
+// 	while (vars->envp[j])
+// 	{
+// 		if (ft_strncmp(vars->envp[j], args[check], ft_strlen(args[check])) == 0)
+// 		{
+// 			return (0);
+// 		}
+// 		j++;
+// 	}
+// 	return (count);
+// }
+
+// void fell_string(t_vars *vars, char **args, int check)
+// {
+// 	char *key = ft_split(args[check], '=')[0];
+// 	vars->strarg = key;
+// 	if (ft_strchr(args[check], '='))
+// 	{
+// 		vars->strvalue = ft_strchr(args[check], '=') + 1;
+// 	}
+// }
+
+void check_unset(char **args, t_vars *vars, int check)
+{
+	int i = 0;
+	char *key;
+
+	key = args[check];
+	while (i < count_argiment(vars->envp))
+	{
+		// printf ("%s\n", vars->env[i].key);
+		// printf("ana hna\n");
+		int ma = ft_strncmp(vars->env[i].key, key, ft_strlen(key));
+		if (ma == 0)
+		{
+			while (i < count_argiment(vars->envp))
+			{
+				vars->envp[i] = vars->envp[i + 1];
+				i++;
+			}
+			vars->envp[i] = 0;
+		}
+		// {
+		// 	i = 0;
+		// 	// return (1);
+		// }
+		i++;
+	}
+}
+
 int check_argiment(char **args, t_vars *vars, int check)
 {
-	int count = 0;
-	if (args[check])
+	int i = 0;
+	char *key;
+	char *value;
+
+	key = ft_split(args[check], '=')[0];
+	printf ("%s\n", key);
+	value =ft_strchr(args[check], '=') + 1;
+	printf ("%s\n", value);
+	while (i < count_argiment(vars->envp))
 	{
-		int i = 0;
-		while (args[check][i])
+		printf ("%s\n", vars->env[i].key);
+		if (ft_strncmp(key, vars->env[i].key, ft_strlen(vars->env[i].key) == 0)
+			&& ft_strncmp(value, vars->env[i].value, ft_strlen(vars->env[i].value) != 0))
 		{
-			if (args[check][i] == '=' && args[check][i + 1] != '\0')
-			{
-				count++;
-			}
-			i++;
+			// vars->env[i].value = value;
+			return (1);
 		}
+		i++;
 	}
-	int j = 0;
-	while (vars->envp[j])
-	{
-		if (ft_strncmp(vars->envp[j], args[check], ft_strlen(args[check])) == 0)
-		{
-			return (0);
-		}
-		j++;
-	}
-	return (count);
+	return (0);
 }
 
-void export_cmd(t_vars *vars, char **args) {
-    int count = 0;
-	while (vars->envp[count])
-    {
-        printf ("%s\n", vars->envp[count]);
-        count++;
-    }
-    if (vars->count_argiment > 1)
-    {
-        int i = 1;
-        while (args[i])
-        {
-            if (ft_strchr(args[i], '=') != NULL)
-            {
-                // char var=
-                vars->envp[count] = args[i];
-                count++;
-                printf ("%s\n", args[i]);
-            }
-            i++;
-        }
-        vars->envp[count] = 0;
-    }
-}
-
-void env_cmd(t_vars *vars) {
+void fell_env_struct(t_vars *vars)
+{
     int i = 0;
-    while (i < count_argiment(vars->envp)) {
-        if (vars->env[i].is_equal) {
+    char *key;
+    while (vars->envp[i])
+	{
+        key = ft_split(vars->envp[i], '=')[0];
+        vars->env[i].key = key;
+        if (ft_strchr(vars->envp[i], '='))
+		{
+            vars->env[i].is_equal = 1;
+            vars->env[i].value = ft_strchr(vars->envp[i], '=') + 1;
+        }
+        i++;
+    }
+}
+
+void env_cmd(t_vars *vars)
+{
+    int i = 0;
+    while (i < count_argiment(vars->envp))
+	{
+        if (vars->env[i].is_equal)
+		{
             printf("%s", vars->env[i].key);
             printf("=");
             printf("%s\n", vars->env[i].value);
@@ -113,6 +170,74 @@ void env_cmd(t_vars *vars) {
         i++;
     }
 }
+
+void export_cmd(t_vars *vars, char **args)
+{
+    int count = 0;
+	
+	while (count < count_argiment(vars->envp))
+	{
+        count++;
+	}
+	if (vars->count_argiment > 1)
+    {
+        int i = 1;
+        while (args[i])
+        {
+			// if (check_argiment(args, vars, i) == 0)
+			// {
+				
+			// }
+            if (ft_strchr(args[i], '=') != NULL)
+            {
+                vars->envp[count] = args[i];
+                count++;
+            }
+            i++;
+        }
+        vars->envp[count] = 0;
+    }
+	count = 0;
+	if (vars->count_argiment == 1)
+	{
+		while (count < count_argiment(vars->envp))
+		{
+			if (vars->env[count].is_equal)
+			{
+				printf("%s", vars->env[count].key);
+				printf("=");
+				printf("%s\n", vars->env[count].value);
+			}
+			count++;
+		}
+	}
+}
+
+// void export_cmd(t_vars *vars, char **args)
+// {
+//     int count = 0;
+// 	while (vars->envp[count])
+//     {
+//         printf ("%s\n", vars->envp[count]);
+//         count++;
+//     }
+//     if (vars->count_argiment > 1)
+//     {
+//         int i = 1;
+//         while (args[i])
+//         {
+//             if (ft_strchr(args[i], '=') != NULL)
+//             {
+//                 vars->envp[count] = args[i];
+//                 count++;
+//                 printf ("%s\n", args[i]);
+//             }
+//             i++;
+//         }
+//         vars->envp[count] = 0;
+//     }
+// }
+
 
 void run(char *cmd, char **args, t_vars *vars)
 {
@@ -145,11 +270,6 @@ void run(char *cmd, char **args, t_vars *vars)
 	
     else if (ft_strncmp(cmd, "pwd", ft_strlen("pwd")) == 0)
     {
-        // if (cwd == NULL)
-        // {
-        //     printf ("Get current working directory failed.\n");
-        //     return ;
-        // }
 		if (vars->count_argiment > 1)
 			printf ("pwd: too many arguments\n");
 		else
@@ -162,36 +282,38 @@ void run(char *cmd, char **args, t_vars *vars)
 	}
 	
 	else if (ft_strncmp(cmd, "env", ft_strlen("env")) == 0)
+	{
 		env_cmd(vars);
-	
+	}
 	else if (ft_strncmp(cmd, "unset", ft_strlen("unset")) == 0)
 	{
 		int i = 1;
-		int j;
+		// int j;
 		while (args[i])
 		{
-			j = 0;
-			while (vars->envp[j])
-			{
-				if (ft_strncmp(vars->envp[j], args[i], ft_strlen(args[i])) == 0)
-				{
-					while (vars->envp[j])
-					{
-						vars->envp[j] = vars->envp[j + 1];
-						j++;
-					}
-					vars->envp[j] = 0;
-				}
-				j++;
-			}
+			check_unset(args, vars, i);
+		// 	j = 0;
+		// 	while (vars->envp[j])
+		// 	{
+		// 		if (ft_strncmp(vars->envp[j], args[i], ft_strlen(args[i])) == 0)
+		// 		{
+		// 			while (vars->envp[j])
+		// 			{
+		// 				vars->envp[j] = vars->envp[j + 1];
+		// 				j++;
+		// 			}
+		// 			vars->envp[j] = 0;
+		// 		}
+		// 		j++;
+		// 	}
 			i++;
 		}
-		j = 0;
-		while (vars->envp[j])
-		{
-			printf ("%s\n", vars->envp[j]);
-			j++;
-		}
+		// j = 0;
+		// while (vars->envp[j])
+		// {
+		// 	printf ("%s\n", vars->envp[j]);
+		// 	j++;
+		// }
 	}
 
 	
@@ -201,19 +323,6 @@ void run(char *cmd, char **args, t_vars *vars)
     }
 }
 
-void fell_env_struct(t_vars *vars) {
-    int i = 0;
-    char *key;
-    while (vars->envp[i]) {
-        key = ft_split(vars->envp[i], '=')[0];
-        vars->env[i].key = key;
-        if (ft_strchr(vars->envp[i], '=')) {
-            vars->env[i].is_equal = 1;
-            vars->env[i].value = ft_strchr(vars->envp[i], '=') + 1;
-        }
-        i++;
-    }
-}
 
 int main(int c, char **v, char **env)
 {
@@ -225,9 +334,9 @@ int main(int c, char **v, char **env)
 	(void)c;
 	vars.envp = env;
     vars.env = malloc(sizeof(t_env) * (count_argiment(vars.envp)));
-    fell_env_struct(&vars);
     while(1)
     {
+    	fell_env_struct(&vars);
         input = read_input();
         str = ft_split(input, ' ');
         char* command = str[0];
--- minishell.h
+++ minishell.h
@@ -6,14 +6,12 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:31:58 by emohamed          #+#    #+#             */
-/*   Updated: 2023/09/06 17:26:46 by haarab           ###   ########.fr       */
+/*   Updated: 2023/09/07 13:03:44 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
-
-// int system(const char* command);
 
 
 #include <stdio.h>
@@ -37,21 +35,15 @@ typedef struct s_vars
 {
 	int	count_argiment;
 	char **envp;
+	char *strarg;
+	char *strvalue;
     t_env *env;
-    }	t_vars;
+}	t_vars;
 
     void display_prompt();
     char *read_input();
     char	**ft_split(char const *s, char c);
     char **split_input(char *str);
     int	count_s_lenght(const char *s, char c);
-    // size_t	ft_strlen(const char *s);
-    // size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
-    // int		ft_atoi(const char *str);
-    // void	*ft_calloc(size_t count, size_t size);
-    // char	*ft_strdup(const char *s1);
-    // char	*ft_substr(char const *s, unsigned int start, size_t len);
-    // char	*ft_strjoin(char const *s1, char const *s2);
-    // char	*ft_itoa(int n);
 
 #endif
