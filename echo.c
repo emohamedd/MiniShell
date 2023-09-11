92857a30270e39b3bcddd3bbdd780792fb5e3515
Hamza Aarab <haarab@c1r4p3.1337.ma>
2023-09-11 14:36:20 +0100

    update

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 19:49:04 by haarab            #+#    #+#             */
-/*   Updated: 2023/09/10 22:41:14 by haarab           ###   ########.fr       */
+/*   Updated: 2023/09/11 11:38:59 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,11 @@ void run_echo (char **args, t_vars *vars)
     int i = 1;
     int check = 0;
     // int g = 1;
-
+	// if (args[1] == NULL)
+	// 	return;
     while (args[i])
     {
+		// printf ("string ==== %s\n", args[i]);
         // if (args[i])
             // g = ft_strncmp(args[i], "-n", ft_strlen("-n"));
             // g = check_n(args[i]);
@@ -49,14 +51,14 @@ void run_echo (char **args, t_vars *vars)
             i++;
         }
 		if (check_n(args[i]) == 1)
-			vars->exit_status = 1;
-        if (args[i])
+			vars->exit_status = 0;
+        while (args[i])
 		{
             printf("%s", args[i]);
+			if (args[i] != NULL && args[i + 1] != NULL)
+				printf(" ");
+        	i++;
 		}
-		if (args[i] != NULL && args[i + 1] != NULL)
-			printf(" ");
-        i++;
     }
     if (check == 0)
         printf("\n");
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: haarab <haarab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 13:10:25 by emohamed          #+#    #+#             */
-/*   Updated: 2023/09/10 22:54:36 by haarab           ###   ########.fr       */
+/*   Updated: 2023/09/11 12:55:49 by haarab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,16 +63,17 @@ char *get_path(t_vars *vars, char *cmd)
 	res = ft_getenv("PATH", vars);
 	if(res == NULL)
 		return (NULL);
+	char *fullCmd;
 	path = ft_split(res, ':');
 	struct stat file_info;
 	while (path[i]) {
-		char *fullCmd = ft_strjoin(ft_strjoin(path[i], "/"), cmd);
+		fullCmd = ft_strjoin(ft_strjoin(path[i], "/"), cmd);
 		if (stat(fullCmd, &file_info) == 0) {
 			return fullCmd;
 		}
 		i++;
 	}
-	return (res);
+	return (NULL);
 }
 
 void run(char *cmd, char **args, t_vars *vars)
@@ -81,14 +82,14 @@ void run(char *cmd, char **args, t_vars *vars)
 
     if (ft_strncmp(cmd, "echo", ft_strlen("echo")) == 0)
     {
-		
+		// int kl = 2;
         run_echo(args, vars);
-		int kl = ft_strncmp(("$?"), args[1], ft_strlen(args[1]) == 0);
+		// kl = ft_strncmp(("$?"), args[1], ft_strlen(args[1]) == 0);
 		// printf("%d\n", kl);
-		if (kl == 0)
-		{
-			printf("%d\n", vars->exit_status);
-		}
+		// if (kl == 0)
+		// {
+		// 	printf("%d\n", vars->exit_status);
+		// }
 		// printf ("%s\n", args[1]);
 		// if (ft_strncmp(args[1], "$?", ft_strlen("$?")) == 0)
 			// printf ("exit status %d\n", vars->exit_status);
@@ -148,18 +149,27 @@ void run(char *cmd, char **args, t_vars *vars)
 
 void exec_cmds(t_vars *vars, char *cmd, char **args) {
 	int id;
+	char *path;
+	path = get_path(vars, cmd);
 
 	id = fork();
-	if (id == 0) {
-		if (execve(get_path(vars, cmd), args, vars->envp) == -1 ) {
-			ft_putstr_fd("minishell : ", 2);
-			ft_putstr_fd(cmd, 2);
-			ft_putstr_fd(": command not found\n", 2);
+	if (id == 0) 
+	{
+		if (path == NULL)
+		{
+			ft_putstr_fd("command not found: ", 2);
+			ft_putendl_fd(cmd, 2);
 			exit(127);
+			
 		}
-	}else {
-		waitpid(id, NULL, 0);
+		else if (execve(path, args, vars->envp) == -1 )
+		{
+			perror("execve");
+			exit(126);
+		}
 	}
+	wait(&vars->exit_status);
+	vars->exit_status = WEXITSTATUS(vars->exit_status);
 }
 
 char	**get_cmds(t_info **info)
@@ -178,8 +188,6 @@ char	**get_cmds(t_info **info)
 	}
 	return (ft_split(cmd, ' '));
 }
-
-
 int main(int c, char **v, char **env)
 {
 	char **str;
